a4736c2bc35b786869aa50c9dbfc8bc1b82fb6c3
francisco-solis99 <franciscosoliscss@gmail.com>
2023-12-27 15:44:27 -0600

    feat: :sparkles: #day1-add the initial gifs to the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
+import { useState } from 'react'
 import './App.css'
 
+const INITIAL_GIFTS = ['Medias', 'Cramelos', 'Vitel Tone']
+
 function App() {
+  const [gifts, setGifts] = useState(INITIAL_GIFTS)
+
   return (
     <>
-      <div>
-        Mi app
+      <div className='app__wrapper'>
+        <div className='gifts__info'>
+          <h1>Regalos</h1>
+
+          <ul className='gifts__list'>
+            {
+              gifts.map(gift => (
+                <li key={gift}>{gift}</li>
+              ))
+            }
+          </ul>
+        </div>
       </div>
     </>
   )
