be3ea660357932b5107d0a844e6560193e466a9c
francisco-solis99 <franciscosoliscss@gmail.com>
2023-12-27 21:24:11 -0600

    feat: :sparkles: #day-5 add the default message when there are not gifts

--- src/components/ListGifts.jsx
+++ src/components/ListGifts.jsx
@@ -4,11 +4,17 @@
 export function ListGifs({gifts, deleteGiftById}) {
 
   const handleClickDeleteGift = (id) => deleteGiftById(id)
-
+  const isEmpty = gifts.length === 0;
 
   return (
     <ul className='gifts__list'>
             {
+              isEmpty
+                ?
+                  <li className="gifts__empty">
+                    No hay regalos, agrega alguno
+                  </li>
+                :
                   gifts.map(gift => (
                     <li key={gift.id} className='gift'>
                       <span>
